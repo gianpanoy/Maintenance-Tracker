7fb88191673aae537dd48c37e5e52af1c1ff4a54
Gian <gckp@hawaii.edu>
2026-06-30 14:49:43 -1000

    Added Footer

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import "./globals.css"
 import Link from "next/link"
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = { title: "Maintenance Tracker" }
 
@@ -21,6 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </nav>*/}
 
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   )
---
+++ frontend/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-slate-900 text-gray-400 py-4 text-center text-sm border-t border-slate-800">
+      © 2026 Gian Panoy, Intern at Hawaii Department of Transportation. All rights reserved.
+    </footer>
+  );
+}
